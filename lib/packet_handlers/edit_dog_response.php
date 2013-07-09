34b61f4a37fd2203f75957df2e2a7cb6320b1a75
Kruithne <kruithne@gmail.com>
2013-07-09 20:34:40 +0100

    No longer need the orignal values, just an ID to edit.

--- lib/packet_handlers/edit_dog_response.php
+++ lib/packet_handlers/edit_dog_response.php
@@ -3,10 +3,9 @@
 	{
 		public function Run()
 		{
-			$orig_pattern = REST::Get('orig_pattern');
-			$orig_reply = REST::Get('orig_reply');
-			$new_pattern = REST::Get('new_pattern');
-			$new_reply = REST::Get('new_reply');
+			$id = REST::Get('response_id');
+			$pattern = REST::Get('response_pattern');
+			$reply = REST::Get('response_reply');
 
 			// ToDo: Handle this.
 		}
