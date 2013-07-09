c54b217136083607c38d86e4b193ba557865435a
Kruithne <kruithne@gmail.com>
2013-07-09 20:35:50 +0100

    Make sure we are both logged in and an admin before allowing the packet to process.

--- lib/packet_handlers/edit_dog_response.php
+++ lib/packet_handlers/edit_dog_response.php
@@ -3,13 +3,16 @@
 	{
 		public function Run()
 		{
-			$id = REST::Get('response_id');
-			$pattern = REST::Get('response_pattern');
-			$reply = REST::Get('response_reply');
-
-			if ($id != null && $pattern != null && $reply != null)
+			if (Authenticator::IsLoggedIn() && Authenticator::IsAdmin())
 			{
-				// ToDo: Handle this.
+				$id = REST::Get('response_id');
+				$pattern = REST::Get('response_pattern');
+				$reply = REST::Get('response_reply');
+
+				if ($id != null && $pattern != null && $reply != null)
+				{
+					// ToDo: Handle this.
+				}
 			}
 		}
 	}
