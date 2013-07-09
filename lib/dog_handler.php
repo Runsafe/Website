54f72a8cb373f892fe8394a4995278ae4ebf4bb3
Kruithne <kruithne@gmail.com>
2013-07-09 04:35:12 +0100

    Added function to pull all of the DOG patterns from the DB

--- lib/dog_handler.php
+++ lib/dog_handler.php
@@ -1,6 +1,12 @@
 <?php
 	class DogHandler
 	{
+		public static function GetAllResponses()
+		{
+			$query = DB::prepare('SELECT pattern, reply FROM ai_dog');
+			$query->execute();
 
+			return DB::prepareObjects($query);
+		}
 	}
 ?>
