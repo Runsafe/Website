c42515698d4a105c47a62e79986611f70d8ed700
Kruithne <kruithne@gmail.com>
2013-07-09 20:33:10 +0100

    Include ID when pulling DOG responses from the database.

--- lib/dog_handler.php
+++ lib/dog_handler.php
@@ -3,7 +3,7 @@
 	{
 		public static function GetAllResponses()
 		{
-			$query = DB::prepare('SELECT pattern, reply FROM ai_dog');
+			$query = DB::prepare('SELECT ID, pattern, reply FROM ai_dog');
 			$query->execute();
 
 			return DB::prepareObjects($query);
