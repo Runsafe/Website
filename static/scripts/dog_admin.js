8bf96e34dd9369e69cec41770c34112a74ea438f
Kruithne <kruithne@gmail.com>
2013-07-09 19:05:29 +0100

    Moved all dog_admin functions into the object constructor to make cleaner code.

--- static/scripts/dog_admin.js
+++ static/scripts/dog_admin.js
@@ -1,30 +1,26 @@
-var dog_admin = {};
-
-dog_admin.focusResponse = function()
-{
-	dog_admin.getResponseInputs(this).css('border', '1px solid black');
-};
-
-dog_admin.blurResponse = function()
-{
-	dog_admin.getResponseInputs(this).css('border', '1px solid #B8B8B8');
-};
-
-dog_admin.editResponse = function()
-{
-	dog_admin.getResponseInputs(this).css('background-color', '#DBA2A2');
-};
-
-dog_admin.getResponseInputs = function(obj)
-{
-	return $(obj).parent().children('input');
-};
-
-dog_admin.load = function()
-{
-	$(document).on('focus', '.dog-response input', dog_admin.focusResponse);
-	$(document).on('blur', '.dog-response input', dog_admin.blurResponse);
-	$(document).on('keydown', '.dog-response input', dog_admin.editResponse);
+var dog_admin = {
+	focusResponse: function()
+	{
+		dog_admin.getResponseInputs(this).css('border', '1px solid black');
+	},
+	blurResponse: function()
+	{
+		dog_admin.getResponseInputs(this).css('border', '1px solid #B8B8B8');
+	},
+	editResponse: function()
+	{
+		dog_admin.getResponseInputs(this).css('background-color', '#DBA2A2');
+	},
+	getResponseInputs: function()
+	{
+		return $(obj).parent().children('input');
+	},
+	load: function()
+	{
+		$(document).on('focus', '.dog-response input', dog_admin.focusResponse);
+		$(document).on('blur', '.dog-response input', dog_admin.blurResponse);
+		$(document).on('keydown', '.dog-response input', dog_admin.editResponse);
+	}
 };
 
 $(document).ready(dog_admin.load);
