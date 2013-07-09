f594b1427115500916bbfe74bd82a8315e2d132c
Kruithne <kruithne@gmail.com>
2013-07-09 19:29:00 +0100

    Added save feature, requires hookup to the server.

--- static/scripts/dog_admin.js
+++ static/scripts/dog_admin.js
@@ -6,21 +6,34 @@ var dog_admin = {
 	blurResponse: function()
 	{
 		dog_admin.getResponseInputs(this).css('border', '1px solid #B8B8B8');
+		dog_admin.saveResponses();
 	},
 	editResponse: function()
 	{
+		dog_admin.edited.push(this);
 		dog_admin.getResponseInputs(this).css('background-color', '#DBA2A2');
 	},
-	getResponseInputs: function()
+	getResponseInputs: function(obj)
 	{
 		return $(obj).parent().children('input');
+	},
+	saveResponses: function()
+	{
+		for (node_pointer in dog_admin.edited)
+		{
+			var node = dog_admin.edited[node_pointer];
+			var inputs = dog_admin.getResponseInputs(node);
+			inputs.css('background-color', 'white');
+		}
+		dog_admin.edited = [];
 	},
 	load: function()
 	{
 		$(document).on('focus', '.dog-response input', dog_admin.focusResponse);
 		$(document).on('blur', '.dog-response input', dog_admin.blurResponse);
 		$(document).on('keydown', '.dog-response input', dog_admin.editResponse);
-	}
+	},
+	edited: []
 };
 
 $(document).ready(dog_admin.load);
