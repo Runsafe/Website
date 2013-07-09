5c4d758d526e0f8fb9b820544c2edb48ed430009
Morten Nilsen <morten@runsafe.no>
2013-07-09 22:36:25 +0200

    Apply jQuery conventions for document.ready callbacks

--- static/scripts/login.js
+++ static/scripts/login.js
@@ -1,68 +1,70 @@
-var LoginHandler =
-{
-	login: function()
+$(function(){ 
+	var LoginHandler =
 	{
-		console.log(LoginHandler);
-		var username = LoginHandler.usernameField.val(),
-			password = LoginHandler.passwordField.val();
-
-		if (username.length < 1 || password.length < 1)
+		login: function()
 		{
-			LoginHandler.showError('Please enter both your username and password.');
-			return;
-		}
+			console.log(LoginHandler);
+			var username = LoginHandler.usernameField.val(),
+				password = LoginHandler.passwordField.val();
 
-		PacketHandler.send(Packet.Login, {username: username, password: password});
-	},
-	handleLoginResponse: function(data)
-	{
-		if (data.response != undefined)
+			if (username.length < 1 || password.length < 1)
+			{
+				LoginHandler.showError('Please enter both your username and password.');
+				return;
+			}
+
+			PacketHandler.send(Packet.Login, {username: username, password: password});
+		},
+		handleLoginResponse: function(data)
 		{
-			if (data.response == 0)
-				LoginHandler.showError('Invalid username and/or password.');
+			if (data.response != undefined)
+			{
+				if (data.response == 0)
+					LoginHandler.showError('Invalid username and/or password.');
+				else
+				{
+					LoginHandler.hideError();
+					window.location = 'index.php';
+				}
+			}
 			else
 			{
-				LoginHandler.hideError();
-				window.location = 'index.php';
+				this.showError('There was an unknown error, please try again later.');
 			}
+		},
+		showError: function(error)
+		{
+			LoginHandler.errorBox.html(error);
+			LoginHandler.errorBox.slideDown();
+		},
+		hideError: function()
+		{
+			LoginHandler.errorBox.slideUp();
+		},
+		onLoad: function()
+		{
+			LoginHandler.usernameField = $('#username-field');
+			LoginHandler.passwordField = $('#password-field');
+
+			LoginHandler.usernameField.on('keypress', function(data)
+			{
+				if (data.keyCode == 13)
+					LoginHandler.passwordField.focus();
+			});
+
+
+			LoginHandler.passwordField.on('keypress', function(data)
+			{
+				if (data.keyCode == 13)
+					LoginHandler.login();
+			});
+
+			LoginHandler.errorBox = $('#login-notice');
+
+			PacketHandler.hook(Packet.Login, LoginHandler.handleLoginResponse);
+			$(document).on('click', '#login-button', LoginHandler.login);
 		}
-		else
-		{
-			this.showError('There was an unknown error, please try again later.');
-		}
-	},
-	showError: function(error)
-	{
-		LoginHandler.errorBox.html(error);
-		LoginHandler.errorBox.slideDown();
-	},
-	hideError: function()
-	{
-		LoginHandler.errorBox.slideUp();
-	},
-	onLoad: function()
-	{
-		LoginHandler.usernameField = $('#username-field');
-		LoginHandler.passwordField = $('#password-field');
+	};
 
-		LoginHandler.usernameField.on('keypress', function(data)
-		{
-			if (data.keyCode == 13)
-				LoginHandler.passwordField.focus();
-		});
-
-
-		LoginHandler.passwordField.on('keypress', function(data)
-		{
-			if (data.keyCode == 13)
-				LoginHandler.login();
-		});
-
-		LoginHandler.errorBox = $('#login-notice');
-
-		PacketHandler.hook(Packet.Login, LoginHandler.handleLoginResponse);
-		$(document).on('click', '#login-button', LoginHandler.login);
-	}
-};
-
-$(document).ready(LoginHandler.onLoad);
+	LoginHandler.onLoad();
+});
--- static/scripts/spleef.js
+++ static/scripts/spleef.js
@@ -1,28 +1,28 @@
-var Spleef = {};
+$(function(){
+	var Spleef = {
+		checkScore: function()
+		{
+			var playerName = Spleef.playerNameField.val();
+			if (playerName.length > 0)
+				PacketHandler.send(Packet.SpleefScore, { playerName: playerName });
+		},
 
-Spleef.checkScore = function()
-{
-	var playerName = Spleef.playerNameField.val();
-	if (playerName.length > 0)
-		PacketHandler.send(Packet.SpleefScore, { playerName: playerName });
-};
+		handlerScoreReturn: function(data)
+		{
+			if (data.score != undefined)
+				Spleef.outputField.html('<img src="http://minecraft.runsafe.no/map/tiles/faces/16x16/{0}.png"/>{0} has {1} wins in spleef!'.format(data.playerName, data.score));
+		},
 
-Spleef.handlerScoreReturn = function(data)
-{
-	if (data.score != undefined)
-		Spleef.outputField.html('<img src="http://minecraft.runsafe.no/map/tiles/faces/16x16/{0}.png"/>{0} has {1} wins in spleef!'.format(data.playerName, data.score));
-};
+		playerNameField: $('#player'),
+		outputField: $('#spleef-lookup')
+	};
 
-$(document).ready(function()
-{
+
 	PacketHandler.hook(Packet.SpleefScore, Spleef.handlerScoreReturn);
-	Spleef.playerNameField = $('#player');
 
 	Spleef.playerNameField.on('keypress', function(data)
 	{
 		if (data.keyCode == 13)
 			Spleef.checkScore();
 	});
-
-	Spleef.outputField = $('#spleef-lookup');
 });
