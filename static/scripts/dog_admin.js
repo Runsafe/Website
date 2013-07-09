c35b538a03676a307491d1737882700e0bdcf6f7
Kruithne <kruithne@gmail.com>
2013-07-09 19:02:42 +0100

    Added highlight to unedited fields and worked object getter into seperate function.

--- static/scripts/dog_admin.js
+++ static/scripts/dog_admin.js
@@ -2,19 +2,29 @@ var dog_admin = {};
 
 dog_admin.focusResponse = function()
 {
-	$(this).parent().children('input').css('border', '1px solid black');
+	dog_admin.getResponseInputs(this).css('border', '1px solid black');
 };
 
 dog_admin.blurResponse = function()
 {
-	$(this).parent().children('input').css('border', '1px solid #B8B8B8');
+	dog_admin.getResponseInputs(this).css('border', '1px solid #B8B8B8');
 };
 
+dog_admin.editResponse = function()
+{
+	dog_admin.getResponseInputs(this).css('background-color', '#DBA2A2');
+};
+
+dog_admin.getResponseInputs = function(obj)
+{
+	return $(obj).parent().children('input');
+};
 
 dog_admin.load = function()
 {
 	$(document).on('focus', '.dog-response input', dog_admin.focusResponse);
 	$(document).on('blur', '.dog-response input', dog_admin.blurResponse);
+	$(document).on('keydown', '.dog-response input', dog_admin.editResponse);
 };
 
 $(document).ready(dog_admin.load);
