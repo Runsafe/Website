920ed7612d1149f50f8c1a1bba64980c7b1cddda
Kruithne <kruithne@gmail.com>
2013-07-09 18:56:03 +0100

    Added dual border highlighting when selecting a response to edit.

--- static/scripts/dog_admin.js
+++ static/scripts/dog_admin.js
@@ -1,1 +1,20 @@
 var dog_admin = {};
+
+dog_admin.focusResponse = function()
+{
+	$(this).parent().children('input').css('border', '1px solid black');
+};
+
+dog_admin.blurResponse = function()
+{
+	$(this).parent().children('input').css('border', '1px solid #B8B8B8');
+};
+
+
+dog_admin.load = function()
+{
+	$(document).on('focus', '.dog-response input', dog_admin.focusResponse);
+	$(document).on('blur', '.dog-response input', dog_admin.blurResponse);
+};
+
+$(document).ready(dog_admin.load);
