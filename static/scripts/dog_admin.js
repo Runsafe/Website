d61bec447ecda2b750ee7ba1eb26c7ce1462cade
Kruithne <kruithne@gmail.com>
2013-07-09 04:02:46 +0100

    Added blank JS library for dog_admin

---
+++ static/scripts/dog_admin.js
@@ -0,0 +1,1 @@
+var dog_admin = {};
--- templates/dog_admin.php
+++ templates/dog_admin.php
@@ -1,2 +1,3 @@
+<script type="text/javascript" src="http://static.minecraft.runsafe.no/scripts/dog_admin.js"></script>
 <h1>Dog Administration</h1>
 <p>Placeholder</p>
