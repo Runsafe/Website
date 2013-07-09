c372471f09576b13f3b0c11e18ba3ed5b0d7db14
Kruithne <kruithne@gmail.com>
2013-07-09 18:37:10 +0100

    dog_admin page now outputs a basic list of DOG responses and patterns.

--- templates/dog_admin.php
+++ templates/dog_admin.php
@@ -1,3 +1,27 @@
 <script type="text/javascript" src="http://static.minecraft.runsafe.no/scripts/dog_admin.js"></script>
 <h1>Dog Administration</h1>
-<p>Placeholder</p>
+<?php
+	if ($this->responses != null)
+	{
+		if (count($this->responses))
+		{
+			foreach ($this->responses as $response)
+			{
+				?>
+					<div class="dog-response">
+						<input type="text" class="pattern" value="<?php echo $response->pattern; ?>"/>
+						<input type="text" class="response" value="<?php echo $response->response; ?>"/>
+					</div>
+				<?php
+			}
+		}
+		else
+		{
+			?><p>There are no DOG responses, try adding one?</p><?php
+		}
+	}
+	else
+	{
+		?><p>Sorry, we could not produce a list of DOG responses right now!</p><?php
+	}
+?>
