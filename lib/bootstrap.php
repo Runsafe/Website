4fe6dcee878f47a7f531b6b9f91ded9f102542dc
Kruithne <kruithne@gmail.com>
2013-07-09 04:09:36 +0100

    Cleaned up bootstrap.

--- lib/bootstrap.php
+++ lib/bootstrap.php
@@ -7,27 +7,29 @@
 	require_once('constants.php');
 	require_once('class_loader.php');
 
+	//Core
 	ClassLoader::registerClass('DB', 'database.php');
 	ClassLoader::registerClass('PhorumDB', 'phorum_database.php');
 	ClassLoader::registerClass('REST', 'rest_handler.php');
 	ClassLoader::registerClass('Session', 'session_handler.php');
 	ClassLoader::registerClass('Transporter', 'transporter.php');
-	ClassLoader::registerClass('Module', 'module.php');
-	ClassLoader::registerClass('MemberModule', 'member_module.php');
-	ClassLoader::registerClass('AdminModule', 'admin_module.php');
 	ClassLoader::registerClass('Template', 'template.php');
 	ClassLoader::registerClass('PacketHandler', 'packet_handler.php');
-
 	ClassLoader::registerClass('Authenticator', 'authenticator.php');
 	ClassLoader::registerClass('Transporter', 'transporter.php');
 
-	ClassLoader::registerClass('PVPEvent', 'pvp_event.php');
+	// Module types
+	ClassLoader::registerClass('Module', 'module.php');
+	ClassLoader::registerClass('MemberModule', 'member_module.php');
+	ClassLoader::registerClass('AdminModule', 'admin_module.php');
 
+	// Specific page handlers
 	ClassLoader::registerClass('StaffListHandler', 'staff_list_handler.php');
 	ClassLoader::registerClass('SpleefHandler', 'spleef_handler.php');
 	ClassLoader::registerClass('PvPRankingsHandler', 'pvp_rankings_handler.php');
 	ClassLoader::registerClass('AccountManager', 'account_manager.php');
 	ClassLoader::registerClass('PVPShopHandler', 'pvp_shop_handler.php');
+	ClassLoader::registerClass('PVPEvent', 'pvp_event.php');
 
 	spl_autoload_register('ClassLoader::loadClass');
 ?>
