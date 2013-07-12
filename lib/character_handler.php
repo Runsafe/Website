04baeaa518c1878f2fa0c8129b3b38a293aa3879
Kruithne <kruithne@gmail.com>
2013-07-12 19:56:52 +0100

    Added function to pull correct player name from the DB.

--- lib/character_handler.php
+++ lib/character_handler.php
@@ -1,6 +1,16 @@
 <?php
 	class CharacterHandler
 	{
+		public static function getCorrectCharacterName($playerName)
+		{
+			$query = DB::prepare('SELECT name FROM player_db WHERE LOWER(name) = :player');
+			$query->bindValue(':player', strtolower($playerName));
+			$query->execute();
 
+			if ($player = $query->fetchObject())
+				return $player->name;
+
+			return $playerName;
+		}
 	}
 ?>
