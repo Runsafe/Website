a2cef472284a36402f34920e602460cc2f827c8a
Kruithne <kruithne@gmail.com>
2014-02-22 23:18:43 +0000

    All the links should be HTTPS.

--- templates/runsafe_home.php
+++ templates/runsafe_home.php
@@ -2,9 +2,9 @@
 <div id="main-logos">
 	<a href="https://minecraft.runsafe.no/"><img src="https://static.runsafe.no/images/main_icon_mc.png" alt="Minecraft"/></a>
 	<a href="https://github.com/Runsafe/"><img src="https://static.runsafe.no/images/main_icon_git.png" alt="GitHub"/></a>
-	<a href="http://jenkins.runsafe.no/"><img src="https://static.runsafe.no/images/main_icon_jenkins.png" alt="Jenkins"/></a>
+	<a href="https://jenkins.runsafe.no/"><img src="https://static.runsafe.no/images/main_icon_jenkins.png" alt="Jenkins"/></a>
 	<a href="https://vpn.runsafe.no:943/"><img src="https://static.runsafe.no/images/main_icon_vpn.png" alt="VPN"/></a>
-	<a href="http://mail.skviril.net/"><img src="https://static.runsafe.no/images/main_icon_mail.png" alt="Webmail"/></a>
+	<a href="https://mail.skviril.net/"><img src="https://static.runsafe.no/images/main_icon_mail.png" alt="Webmail"/></a>
 	<a href="https://admin.skviril.net/"><img src="https://static.runsafe.no/images/main_icon_maila.png" alt="Mail Admin"/></a>
 </div>
 <div id="main-logo-text"></div>
