3228bb172ae782fd3c36fe0a8d0710590fa1a1d6
Kruithne <kruithne@gmail.com>
2014-02-22 23:16:42 +0000

    Corrected mail admin URL>

--- templates/runsafe_home.php
+++ templates/runsafe_home.php
@@ -5,6 +5,6 @@
 	<a href="http://jenkins.runsafe.no/"><img src="https://static.runsafe.no/images/main_icon_jenkins.png" alt="Jenkins"/></a>
 	<a href="https://vpn.runsafe.no:943/"><img src="https://static.runsafe.no/images/main_icon_vpn.png" alt="VPN"/></a>
 	<a href="http://mail.skviril.net/"><img src="https://static.runsafe.no/images/main_icon_mail.png" alt="Webmail"/></a>
-	<a href="http://admin.skviril.net/"><img src="https://static.runsafe.no/images/main_icon_maila.png" alt="Mail Admin"/></a>
+	<a href="https://admin.skviril.net/"><img src="https://static.runsafe.no/images/main_icon_maila.png" alt="Mail Admin"/></a>
 </div>
 <div id="main-logo-text"></div>
