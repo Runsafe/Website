07f1f33aa2952535da10c323110c298f5f3767a1
Kruithne <kruithne@gmail.com>
2014-02-22 06:25:27 +0000

    Correctly invalid anchor tag.

--- templates/runsafe_home.php
+++ templates/runsafe_home.php
@@ -1,6 +1,6 @@
 <h1>Welcome to Runsafe - An Entire Internets</h1>
 <div id="main-logos">
-	<a href="https://minecraft.runsafe.no/"></a><img src="https://static.runsafe.no/images/main_icon_mc.png" alt="Minecraft"/></a>
+	<a href="https://minecraft.runsafe.no/"><img src="https://static.runsafe.no/images/main_icon_mc.png" alt="Minecraft"/></a>
 	<a href="https://github.com/Runsafe/"><img src="https://static.runsafe.no/images/main_icon_git.png" alt="GitHub"/></a>
 	<a href="http://jenkins.runsafe.no/"><img src="https://static.runsafe.no/images/main_icon_jenkins.png" alt="Jenkins"/></a>
 	<a href="https://vpn.runsafe.no:943/"><img src="https://static.runsafe.no/images/main_icon_vpn.png" alt="VPN"/></a>
