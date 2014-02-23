6032e6b45f8d9d7a787fce52b119ad6c76be07f9
Kruithne <kruithne@gmail.com>
2014-02-23 22:48:12 +0000

    Added server-patch notes as a temp place-holder.

--- templates/minecraft_main.php
+++ templates/minecraft_main.php
@@ -1,1 +1,26 @@
-Minecraft main!
+<b>General Changes</b><br/>
+- Various internal server errors have been fixed.<br/>
+- Portals no longer randomly stop working due to a permission persistence issue.<br/>
+- Better support for numerical arguments to commands.<br/>
+<br/>
+<b>Fishing</b><br/>
+- Every Monday at 1AM GMT a fishing tournament will be held at spawn. The winner of the event will get an awesome new squid mount which can be used in water!<br/>
+- New special items can be obtained by fishing at spawn!<br/>
+<br/>
+<b>Achievements</b><br/>
+- “The Scavenger” - Fish up one of the rare special items from spawn!<br/>
+- “Fishies be mine! Leave dem fishies!” - “Winner of the weekly fishing tournament!”<br/>
+<br/>
+<b>Staff/Admin Only Changes</b><br/>
+- The kit command now supports tab-complete.<br/>
+- A bug preventing staff from setting player-notes has been resolved.<br/>
+- The “/enchant” command now supports the new Lure/Luck enchants.<br/>
+- The “/enchant” command now supports tab-complete.<br/>
+- The “/enchant” command now supports multiple enchants at once.<br/>
+- To apply an enchant at not max power, add :n to the enchant name, ie.: “/enchant luck:2”<br/>
+- Commands that take an entity name can now be tab-completed.<br/>
+- Teleportation is now slightly smarter when you try teleporting to someone whom is floating in thin air.<br/>
+- /whois now can list the players location and their pex rank(s).<br/>
+- Fixed a bug that would sometimes cause regeneration to fail once a plot was deleted.<br/>
+- The “/setportal” command now takes a permission string as an optional third parameter.<br/>
+- Teleporting to a position now accepts two arguments as an alternate usage.<br/>
