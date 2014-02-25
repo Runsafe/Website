0ad5c15fec3ff5de6fe96b4403971851c2d2aba4
Kruithne <kruithne@gmail.com>
2014-02-25 00:08:18 +0000

    Correctly share the sessions across all sub-domains.

--- engine/bootstrap.php
+++ engine/bootstrap.php
@@ -1,6 +1,9 @@
 <?php
 	require_once('../KrameWork/KrameSystem.php');
 
+	session_name('runsafe');
+	session_set_cookie_params(0, '/', '.runsafe.no');
+
 	$system = new KrameSystem();
 
 	$system->getErrorHandler()->addEmailOutputRecipient('kruithne@runsafe.no');
