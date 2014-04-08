1cc7019caa4e6d68f34dac58df215be4b44ed33f
Kruithne <kruithne@gmail.com>
2014-04-08 14:03:38 +0100

    Change-notes tacky update!

--- templates/minecraft_main.php
+++ templates/minecraft_main.php
@@ -1,3 +1,48 @@
+<h1>14th March 2014</h1><br/>
+<b>General Changes</b><br/>
+- A new world has been added: Azuren! Azuren is Runsafe's version of The End, containing new mobs, dungeons and a custom generation design!<br/>
+- Arrows shot from an explosive bow no longer persist in the world.<br/>
+- New achievement: Suddenly, silverfish! - Activate a dungeon in the Azuren world.<br/>
+<br/>
+<h1>13th March 2014</h1><br/>
+<b>General Changes</b><br/>
+- When joining a clan by being one of the initial players who signed the charter, all pending clan invites are successfully removed.<br/>
+- Squid mounts should no longer drop bacon when they die.<br/>
+- If a squid mount takes more damage than it has health, the excess damage points will be inflicted on the rider.<br/>
+- The squid mount item now has a 5 second cooldown.<br/>
+- If a player is dead, their world tag will display as a grey cross.<br/>
+<br/>
+<b>Technical/Server Changes</b><br/>
+- Fixed a number of issues related to player arguments to commands.<br/>
+<h1>1st March 2014</h1><br/><br/>
+<b>General Changes</b><br/>
+- Scaffolding no longer turns into pistons when used against redstone blocks.<br/>
+- Dergons are now more random about how they choose a target, preventing players from “farming” them as much.<br/>
+- Implemented a “/roll” command.<br/>
+<h1>28th February 2014</h1><br/>
+<b>General Changes</b><br/>
+- Fixed internal server errors.<br/>
+- The PvP arena now uses iron enchanted armour rather than leather armour to improve fight quality.<br/>
+- The default armour suit given when joining PvP now matches the new iron enchanted armour available.<br/>
+- Dergon Thunder no longer triggers in no-pvp zones.<br/>
+- Dergons correctly track damage done to them properly and will award to the correct clan.<br/>
+- Jukeboxes now eject anything inside them when a player tries to play a chord book in them.<br/>
+- When sending mail, your inventory will correctly update to show the cost which was removed.<br/>
+- The server no longer attempts to turn players into items when they are fished with a fishing rod.<br/>
+- The blood effect that is shown when a player steps into a hopper has been dramatically improved.<br/>
+- Dergon Eggs will correctly drop a Dergon Egg item when broken.<br/>
+- Four flags have been removed from the spleef arena to prevent players cheating.<br/>
+<br/>
+<b>Technical/Server Changes</b><br/>
+- Commands that take a world as an argument should now take exact matches before partial matches.<br/>
+- Locked objects are loaded and unloaded with the world.<br/>
+- DOG responders may now be assigned permissions limiting who can trigger them.<br/>
+<br/>
+<b>Staff/Admin Only Changes</b><br/>
+- The mute command now takes an optional duration argument.<br/>
+- It is no longer possible to mute offline players in game.<br/>
+- The “/rank” command may now be used on offline players<br/><br/>
+<h1>24th February 2014</h1><br/>
 <b>General Changes</b><br/>
 - Various internal server errors have been fixed.<br/>
 - Portals no longer randomly stop working due to a permission persistence issue.<br/>
