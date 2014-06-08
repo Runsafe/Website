0014008300dfda2dded931bd683ecc55729806a6
Kruithne <kruithne@gmail.com>
2014-06-08 21:43:39 +0100

    Fix: Load classes before the KrameSystem is set-up to allow for session objects to be correctly loaded.

--- engine/bootstrap.php
+++ engine/bootstrap.php
@@ -1,13 +1,14 @@
 <?php
 	require_once('../KrameWork/KrameSystem.php');
 
+	KW_ClassLoader::addClassPath('../modules');
+	KW_ClassLoader::addClassPath('../engine');
+	KW_ClassLoader::addClassPath('../engine/interfaces');
+
 	session_name('runsafe');
 	session_set_cookie_params(0, '/', '.runsafe.no');
 
 	$system = new KrameSystem();
 
 	$system->getErrorHandler()->addEmailOutputRecipient('kruithne@runsafe.no');
-	$system->addAutoLoadPath('../modules');
-	$system->addAutoLoadPath('../engine');
-	$system->addAutoLoadPath('../engine/interfaces');
 ?>
