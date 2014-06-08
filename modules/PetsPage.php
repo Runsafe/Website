a60230d2a41ab8b4e59d6c99f5a5241295895f32
Kruithne <kruithne@gmail.com>
2014-06-08 23:15:38 +0100

    Change: Pets page should use the minecraft navigation module.

--- modules/PetsPage.php
+++ modules/PetsPage.php
@@ -4,7 +4,7 @@
 		public function __construct()
 		{
 			$template = new KW_Template('../templates/pets.php');
-			parent::__construct('Companion Pets', $template, 'runsafe_navigation.php');
+			parent::__construct('Companion Pets', $template, 'minecraft_navigation.php');
 			$this->addStylesheet('form.css');
 			$this->addStylesheet('pets.css');
 			$this->addScript('forms.js');
