316b49ea6d0b277987a7385740e1f767b39a67b1
ExposedGun316 <exposedgun316@live.co.uk>
2015-03-26 18:57:56 +0000

    Added in forum link

--- templates/minecraft_navigation.php
+++ templates/minecraft_navigation.php
@@ -1,3 +1,4 @@
 <li><a href="https://minecraft.runsafe.no/index.php">News</a></li>
 <li><a href="https://wiki.runsafe.no/index.php/Minecraft">Wiki</a></li>
+<li><a href="https://reddit.com/r/RunsafeMinecraft">Forum</a></li>
 <li><a href="https://minecraft.runsafe.no/pets.php">Pets</a></li>
